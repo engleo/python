2300e397ca5689da72a74738dc616bfc7777b8c4
engleo <engleo.santos@gmail.com>
2023-08-21 23:13:11 -0300

    update

--- poo/conta/cadastrar_conta.py
+++ poo/conta/cadastrar_conta.py
@@ -5,4 +5,3 @@ def cadastrar_conta(numero, titular, saldo, limite):
     conta = Conta(numero, titular, saldo, limite)
     return conta
 
-cadastrar_conta(10, "leo", 5000, 100000)
--- poo/conta/menu.py
+++ poo/conta/menu.py
@@ -1,18 +1,27 @@
 from conta import Conta
 from cadastrar_conta import cadastrar_conta
 
-def iniciar(): 
+def iniciar_cadastrar(): 
     resposta = int(input("Cadastrar nova conta( 1 - Sim / 2 - Não ): "))
     return resposta
 
-resposta = iniciar()
+def acessar_conta():
+    resposta_acessar = int(input("Digite o numero da conta: "))
+    return resposta_acessar
+
+resposta = iniciar_cadastrar()
 numero = 1
+contas = {}
 while (resposta == 1):
     titular = input("Digite o nome do titular: ")
     saldo = int(input("Digite o saldo inicial: "))
     limite = 1000 
     
-    cadastrar_conta(numero, titular, saldo, limite)
+    conta = cadastrar_conta(numero, titular, saldo, limite)
+    contas[numero] = conta
+    numero += 1
+    resposta = iniciar_cadastrar()
 
 
+while (resposta)
     
