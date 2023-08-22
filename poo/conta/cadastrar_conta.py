925d14b0676681b9fdbc7ff7003c6b8bb6032c54
engleo <engleo.santos@gmail.com>
2023-08-21 21:43:58 -0300

    update

--- poo/conta/cadastrar_conta.py
+++ poo/conta/cadastrar_conta.py
@@ -5,6 +5,3 @@ def cadastrar_conta(numero, titular, saldo, limite):
     conta = Conta(numero, titular, saldo, limite)
     return conta
 
-if(__name__ == "__main__"):
-    cadastrar_conta()   
-    
